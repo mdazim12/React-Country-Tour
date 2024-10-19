437a58ca5564c703105ff8307519266655f61c4a
Azim <azimuddin807@gmail.com>
2024-10-19 21:29:08 +0600

    load countriess uinsg useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-
-import './App.css'
+import './App.css';
+import Countries from './components/Countries/Countries';
 
 function App() {
-  
-
   return (
     <>
-    
-      <h1> React World Tour </h1>
-      
-      
+      <h1>React World Tour</h1>
+      <Countries></Countries>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const Countries = () => {
+
+    const [countries, setCountries] = useState([]);
+
+    useEffect(()=> {
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res =>res.json())
+        .then(data => console.log(data))
+
+    },[])
+
+    return (
+        <div>
+            <h1>Hello Countries </h1>
+        </div>
+    );
+};
+
+export default Countries;
